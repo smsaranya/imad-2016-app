22c2d3a229b8351aed402ac82f0a68c6155a43d3
smsaranya <smsaranya@gmail.com>
2016-10-20 11:22:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+var img=document.getElementById("madi");
+var marginLeft=0;
+img.onclick=function()
+{
+    var interval=setInterval(moveRight,100);
+}
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+
