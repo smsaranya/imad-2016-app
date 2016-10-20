a60f7175226c372b2224d9e7445c82a07f7b1b75
smsaranya <smsaranya@gmail.com>
2016-10-20 12:32:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/file1', function (req, res) {
+  res.sendFile("sranya");
+});
+app.get('/file2', function (req, res) {
+  res.sendFile("cse");
+});
 var counter=0;
 app.get('/counter', function (req, res) {
     counter=counter+1;
