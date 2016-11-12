663ea0e240190fa72318ec007ec614a02ffc85f6
smsaranya <smsaranya@gmail.com>
2016-11-13 01:22:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,3 +11,28 @@ function moveRight()
     img.style.marginLeft=marginLeft+'px';
 }
 
+				function validate()
+{
+  
+  var uname=document.getElementById("usname").value;
+  
+  var pass=document.getElementById("pass").value;
+  
+  if(uname=="")
+  {
+    alert("please enter the username");
+  }
+   else if(pass=="")
+  {
+    alert("please enter the password");
+  }
+  else if(uname=="admin" && pass=="admin")
+  {
+   window.location="http://www.nptel.ac.in";
+  }
+  else
+  {
+   alert("Invalid Username/Password");
+  }
+}
+	
